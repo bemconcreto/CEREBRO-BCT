93b28ab24bb295b71a7fefba13767b5bbbfea3e3
Elton Ramaldes <eltonramaldes@MacBook-Pro-de-Elton.local>
2025-12-22 19:54:51 -0300

    fix: tailwind + postcss single source of truth

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export function createSupabaseServerClient() {
+export function createClient() {
   const cookieStore = cookies();
 
   return createServerClient(
@@ -9,13 +9,13 @@ export function createSupabaseServerClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return cookieStore.get(name)?.value;
         },
-        set(name, value, options) {
+        set(name: string, value: string, options: any) {
           cookieStore.set({ name, value, ...options });
         },
-        remove(name, options) {
+        remove(name: string, options: any) {
           cookieStore.set({ name, value: "", ...options });
         },
       },
